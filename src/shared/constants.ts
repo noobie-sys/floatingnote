7298f027e0a07760aa1e152653783f7e8b622feb
noobie-sys <amangupta954055@gmail.com>
2026-06-04 04:09:42 +0530

    feat: add app constants and file path configuration

---
+++ src/shared/constants.ts
@@ -0,0 +1,8 @@
+export const appDirectoryName = 'floating-note'
+export const fileEncoding = 'utf8'
+
+export const autoSavingTime = 300
+export const welcomeNoteFilename = 'Welcome.md'
+
+export const FILE = 'file'
+export const FOLDER = 'folder'
